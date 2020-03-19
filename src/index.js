88eaed044384c75f2888320c2e1c776f279311e2
exelarios <derickwok25@gmail.com>
2020-03-19 00:03:53 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,115 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+
+const key = require("../key.js").token;
+
+let prefix = `!`;
+let parentCategoryID = '639304634149634059';
+
+client.on('ready', () => {
+    console.log("Null is online.");
+    client.user.setActivity('with your grades.', {type: 'PLAYING'})
+})
+
+let inSession = [];
+let owners = [];
+console.log(inSession);
+
+client.on('message', async message => {
+    if (message.content.startsWith(`${prefix}tutor`)) {
+        owners.push(`${message.author}`);
+        if (!checkSession(owners)){
+            await message.guild.channels.create(`${message.author.username}-tutoring`)
+            .then( channel => {
+                inSession.push({'Owner': `${message.author}`, 'Channel': `${channel.id}`}); // Check if there's duplicate for for owners 
+                //channelSession.push(`${channel.id}`);
+                channel.setTopic(`This is ${message.author.username}'s tutoring session. 
+                Once this session has ended please close this channel by saying: !closeSession`);
+                channel.setParent(parentCategoryID);
+                channel.send(`${message.author} your tutoring session has been created. Once your tutoring session has completed, please close this channel by typing: !closeSession`);
+            })
+            .catch( err => {
+                console.log(err);
+            })
+            message.author.lastMessage.delete();
+            //console.log(channel.parentID);
+            //console.log(inSession);
+        } else {
+            message.reply(`you already created a tutoring session.`);
+        }
+    }
+
+
+    if (message.content.startsWith(`${prefix}endSession`)) {
+        if (isSameChannel(inSession, message.channel.id) && isSameOwner(inSession, `${message.author}`)) {
+            console.log("Index: " + findIndexByChannel(inSession, message.channel.id));
+            inSession.splice(findIndexByChannel(inSession, message.channel.id), 1);
+            owners.splice(findIdexByOwner(owners, `${message.author}`), 1)
+            message.channel.delete();
+            console.log(inSession);
+            console.log(owners);
+        }
+    }
+
+
+    if (message.content.startsWith(`${prefix}print`)) {
+            console.log(inSession);
+            console.log(owners);     
+    }
+
+    if (message.content.startsWith("null")) {
+        message.channel.send("Did someone say my name?");
+    }
+  });
+
+let checkSession = args => {
+    let valuesSoFar = Object.create(null);
+    for (let i = 0; i < args.length; i++) {
+        let value = args[i];
+        if (value in valuesSoFar) {
+            return true;
+        }
+        valuesSoFar[value] = true;
+    }
+    return false;
+}
+
+let findIdexByOwner = (args, owner) => {
+    for (let i = 0; i < args.length; i++){
+        if (args[i] == owner) {
+            return i;
+        }
+    }
+}
+
+let findIndexByChannel = (args, channel) => {
+    for (let i = 0; i < args.length; i++){
+        if (args[i].Channel == channel) {
+            return i;
+        }
+    }
+}
+
+let isSameChannel = (args, channelId) => {
+    for (let i = 0; i < args.length; i++){
+        console.log(args[i].Channel == channelId);
+        if (args[i].Channel == channelId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let isSameOwner = (args, owner) => {
+    for (let i = 0; i < args.length; i++){
+        console.log(args[i].Owner == owner);
+        if (args[i].Owner == owner) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+client.login(key);
