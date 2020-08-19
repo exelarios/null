68393501cd3dd5e7f978d3e9c293dcff05f3d36e
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2020-08-19 14:43:56 -0700

    fix privatekey

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
         "project_id": process.env.PROJECT_ID,
-        "private_key": process.env.PRIVATE_KEY.replace(/\n/g, '\n') ,
+        "private_key": JSON.parse(process.env.PRIVATE_KEY).replace(/\\n/g, '\n'),
         "client_email": process.env.CLIENT_EMAIL,
     }),
     databaseURL: "https://null-ba189.firebaseio.com"
