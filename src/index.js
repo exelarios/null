085c119edc4d5a119c0a47bf9d46f767d8326eab
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2020-08-19 13:13:46 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ firebaseAdmin.initializeApp({
         "client_email": process.env.CLIENT_EMAIL,
         "client_id": process.env.CLIENT_ID,
         "auth_uri": process.env.AUTH_URI,
-        "token_uri": process.env.TOKEN_URL,
+        "token_uri": process.env.TOKEN_URI,
         "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
         "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
     }),
